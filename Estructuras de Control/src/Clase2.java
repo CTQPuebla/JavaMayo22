fc8a79f784b8e1866b08b759db2e5ae97169a6c3
jorheca <jorge.hernandez.332b@gmail.com>
2022-05-09 08:30:46 -0500

    Ejercicios semana uno JHC

---
+++ Estructuras de Control/src/Clase2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Clase2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+//		System.out.println("ingresa un numero");
+//		int p = sc.nextInt();
+//		System.out.println(p);
+		
+//		System.out.println("ingresa un nombre");		
+//		String m = sc.nextLine();
+//		System.out.println(m);
+		
+		
+		string
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ practice/src/practice/Lun2.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class Lun2 {
+public static void main(String[] args) {
+	
+	//realiza la resta de dos numeros enteros que sean mayores a 10 y menores a 50
+	
+	int numero1=15;
+	int numero2=11;
+	int resultado= 0;
+	resultado= numero1 - numero2;
+	 
+	
+	System.out.println("el resultado de la resta es:"+resultado);
+	
+	
+	
+	
+	
+			
+}
+}
