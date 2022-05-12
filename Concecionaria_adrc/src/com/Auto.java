61f6b6018fd1b4e09d0745c94a173ae8d887a48d
Donovan83 <arnoldonovan97@gmail.com>
2022-05-12 13:43:42 -0500

    semana uno comit

---
+++ Concecionaria_adrc/src/com/Auto.java
@@ -0,0 +1,45 @@
+package com;
+
+public class Auto {
+
+	String marca, modelo;
+	double precio;
+	
+	public Auto() {
+		
+	}
+
+	public Auto(String marca, String modelo, double precio) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.precio = precio;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	
+
+}
---
+++ Concecionaria_adrc/src/com/Principal.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		Concesionaria conce = new Concesionaria("El auto loco");
+		
+		conce.iniciar();
+
+	}
+
+}
